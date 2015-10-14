804121ae4f46c182dade304989bbda30e54889ae
Mamadou Sy <mamadou@askia.com>
2015-10-14 15:51:43 +0200

    Fix the setup.js for the x86 env

--- setup.js
+++ setup.js
@@ -7,7 +7,11 @@ if (process.arch !== 'x64') {
     fs.unlink('./lib/adxshell/AskCmn.dll', function () {
         fs.rename('./lib/adxshell/AskCmn32.dll', './lib/adxshell/AskCmn.dll');
     });
-    fs.unlink('./lib/adxshell/AskCmn.pdb', function () {
-        fs.rename('./lib/adxshell/AskCmn32.pdb', './lib/adxshell/AskCmn.pdb');
+    fs.unlink('./lib/adxshell/AskCmn.pdb', function (err) {
+        if (!err) {
+            fs.rename('./lib/adxshell/AskCmn32.pdb', './lib/adxshell/AskCmn.pdb');
+        }
     });
 }
+// End of process
+process.exit(0);
